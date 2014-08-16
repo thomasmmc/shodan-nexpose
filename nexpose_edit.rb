0f9dcd85e07c14e38cd61efd717ebf7b7149e112
thomasmmc <thomasmmc@gmail.com>
2014-08-16 19:42:39 -0400

    Refactored and added more comments and exception handling

--- nexpose_edit.rb
+++ nexpose_edit.rb
@@ -1,15 +1,19 @@
 require 'nexpose'
 
 def nexpose_edit_site(instance,username,password,site_id,newip)
-  nsc = Nexpose::Connection.new(instance, username, password)
-  nsc.login
-  puts 'Logged into Nexpose'
-
-  puts "Editing site #{site_id}"
-  site = Nexpose::Site.load(nsc, site_id)
-  newip.each do |ip|
-    site.add_ip(ip)
-  end
+  begin
+    #connecting to nexpose instance
+    nsc = Nexpose::Connection.new(instance, username, password)
+    nsc.login
+    #loading the site we want to edit
+    site = Nexpose::Site.load(nsc, site_id)
+    #adding each new ip to the site
+    newip.each do |ip|
+      site.add_ip(ip)
+    end
+    #Saving the changes to the site
     site.save(nsc)
-  puts 'Edited site successfully'
+  rescue Exception => e
+        return "Nexpose API Error: #{e.to_s}"
+  end
 end
