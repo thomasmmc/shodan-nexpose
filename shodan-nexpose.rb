3c6070b09e7b51302121d5b0f317381cdd245d60
thomasmmc <thomasmmc@gmail.com>
2014-08-16 21:05:11 -0400

    Added the ability to pass the conf file path at run time versus having it hard coded. Also added some comments

--- shodan-nexpose.rb
+++ shodan-nexpose.rb
@@ -3,9 +3,16 @@ require 'yaml'
 require_relative 'shodan_api'
 require_relative 'nexpose_edit'
 
+v1 = ARGV[0]
+if v1.nil?
+  conf_file = "conf.yml"
+else
+  conf_file = v1
+end
+
 #Loading conf variables from YAML file
 begin
-  config = YAML.load_file("conf.yml")
+  config = YAML.load_file(conf_file)
   shodan_api_key = config["shodan_api_key"]
   shodan_search = config["shodan_search"]
   instance_name = config["nexpose_host"]
@@ -17,11 +24,13 @@ rescue ArgumentError => e
   puts "Could not parse conf YAML: #{e.message}"
 end
 
+#Running the method call_shodan which used the shodan gem to run our query
 new_ip = call_shodan(shodan_api_key,shodan_search)
 
 if new_ip.any?
   begin
     puts "New ip has been found, going to update Nexpose"
+    #Running the method which uses the nexpose gem to update our specified site
     nexpose_edit_site(instance_name,username,password,nexpose_site,new_ip)
   rescue ArgumentError => e
     puts "Could not update Nexpose: #{e.message}"
